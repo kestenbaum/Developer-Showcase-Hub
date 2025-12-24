85bfa3a8e3bf2904f16313c28e2b9afa64bb58ac
Oleksii <kespa71for@gmail.com>
2025-12-24 08:33:35 +0100

    update: description hero section

--- frontend/components/sections/Hero.tsx
+++ frontend/components/sections/Hero.tsx
@@ -18,8 +18,15 @@ const Hero = () => {
                     </h1>
 
                     <p className="text-xs text-gray-700 max-w-2xl leading-relaxed">
-                        A Result-Oriented Web Developer building and managing Websites and Web Applications that leads
-                        to the success of the overall product
+                        I'm from Ukraine and have been a web developer for the last 2 years. I believe in strong
+                        dedication in each project of my clients and client's satisfaction is the first concern in my
+                        career.
+                        I'm adequate in the prominent skills HTML, CSS, Javascript, different web hosting sectors and so
+                        on. I can solve problems analytically and can face any complex situation with a molive of fixing
+                        it with total vividness. Also, I handle customers with a friend like manner keeping myself
+                        within professionalism.
+                        You can pick me with an assurance of honest and best services.
+                        Thank You.
                     </p>
 
                     <Link href="#projects">
