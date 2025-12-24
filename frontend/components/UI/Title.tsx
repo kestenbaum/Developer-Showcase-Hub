1383f0711a73e850016d9798190d1e22e88ab710
Oleksii <kespa71for@gmail.com>
2025-12-24 08:42:36 +0100

    style: update size Title

--- frontend/components/UI/Title.tsx
+++ frontend/components/UI/Title.tsx
@@ -1,11 +1,11 @@
 const Title = ({title, description}: { title: string, description: string }) => {
     return (
         <div className="flex flex-col items-center justify-center w-full mb-16">
-            <h2 className="font-extrabold text-4xl text-gray-900 text-center mb-6">
+            <h2 className="font-extrabold text-2xl text-gray-900 text-center mb-6">
                 {title}
             </h2>
             <span className="w-16 h-1.5 bg-blue-600 rounded mb-6"></span>
-            <p className="font-medium text-lg md:text-xl text-center text-gray-600 max-w-3xl leading-relaxed">
+            <p className="text-[14px] font-medium text-center text-gray-600 max-w-3xl leading-relaxed">
                 {description}
             </p>
         </div>
