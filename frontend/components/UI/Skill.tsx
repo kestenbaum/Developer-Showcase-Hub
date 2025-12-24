dc5fa4755d9f87e877e989b71aabab488672e3c2
Oleksii <kespa71for@gmail.com>
2025-12-24 09:01:47 +0100

    refactor: update props for Skill

--- frontend/components/UI/Skill.tsx
+++ frontend/components/UI/Skill.tsx
@@ -1,10 +1,4 @@
-import React, { FC } from 'react';
-
-interface Props {
-    children: string;
-}
-
-const Skill: FC<Props> = ({children}) => {
+const Skill = ({children}: { children: string }) => {
     return (
         <li className="w-fit px-4 py-1 rounded-xl bg-gray-100 text-gray-800 text-xs font-semibold uppercase tracking-wide whitespace-nowrap">
             {children}
