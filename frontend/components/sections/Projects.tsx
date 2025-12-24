69458776570a6eaca5106045032b7eef0d9a1169
Oleksii <kespa71for@gmail.com>
2025-12-24 09:03:10 +0100

    style: update styles portfolio section

--- frontend/components/sections/Projects.tsx
+++ frontend/components/sections/Projects.tsx
@@ -7,12 +7,12 @@ const Projects = () => {
     return (
         <section className="bg-white" id="projects">
             <Container>
-                <div className="pt-20">
+                <div className="pt-20 pb-10">
                     <Title
                         title="Projects"
                         description="Here you will find some of the personal and clients projects that I created with each project containing its own case study"
                     />
-                    <div className="flex flex-col gap-5">
+                    <div className="flex flex-col gap-7">
                         <Card/>
                         <Card/>
                         <Card/>
