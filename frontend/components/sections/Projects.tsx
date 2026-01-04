b19a2e73b7c49dc28beb106dde899078f4b5b04f
Oleksii <kespa71for@gmail.com>
2026-01-04 15:30:31 +0100

    feat: rendering project list

--- frontend/components/sections/Projects.tsx
+++ frontend/components/sections/Projects.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+"use client"
+import React, { useEffect } from "react"
 import Container from "@/components/layout/Container"
 import Card from "@/components/sections/components/Card"
 import Title from "@/components/UI/Title"
+import { useStoreProjects } from "@/store/use-store-projects";
 
 const Projects = () => {
+    const { projects, fetchProjects, error, isLoading } = useStoreProjects();
+
+    useEffect(() => {
+        fetchProjects()
+    }, [])
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error fetching.</div>
+
     return (
         <section className="bg-white" id="projects">
             <Container>
@@ -13,10 +24,17 @@ const Projects = () => {
                         description="Here you will find some of the personal and clients projects that I created with each project containing its own case study"
                     />
                     <div className="flex flex-col gap-7">
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
+                        {projects.map((project) =>
+                            <Card
+                                id={project.id}
+                                key={project.id}
+                                linkUrl={project.linkUrl}
+                                imageUrl={project.imageUrl}
+                                skills={project.skills}
+                                title={project.title}
+                                description={project.description}
+                            />
+                        )}
                     </div>
                 </div>
             </Container>
--- frontend/components/sections/components/Card.tsx
+++ frontend/components/sections/components/Card.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Image from "next/image";
-import TestImage from "@/public/dopefolio.png";
 import Button from "@/components/UI/Button";
 import Skill from "@/components/UI/Skill";
+import { ProjectType } from "@/api/types";
+import Link from "next/link";
 
-const Card = () => {
+
+const Card: FC<ProjectType> = ({linkUrl, imageUrl, skills, title, description}) => {
     return (
         <div className="grid grid-cols-1 gap-2.5 sm:grid-cols-2">
             <Image
-                src={TestImage}
+                src={imageUrl ?? '/dopelfolio.png'}
                 alt="image"
                 width="400"
                 height="400"
             />
             <div className="flex flex-col gap-2.5">
-                <h3 className="text-black text-xl font-bold">Dopefolio</h3>
-                <p className="text-black text-[16px] font-medium">Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally</p>
+                <h3 className="text-black text-xl font-bold">{title}</h3>
+                <p className="text-black text-[16px] font-medium">{description}</p>
                 <ul className="flex flex-wrap gap-2.5">
-                    <Skill>HTML</Skill>
-                    <Skill>CSS</Skill>
-                    <Skill>Javascript</Skill>
-                    <Skill>React</Skill>
+                    {skills.map(skill => <Skill key={skill.id}>{skill.title}</Skill>)}
                 </ul>
-                <Button>Case Study</Button>
+                <Link href={linkUrl}>
+                    <Button>Case Study</Button>
+                </Link>
             </div>
         </div>
     );
